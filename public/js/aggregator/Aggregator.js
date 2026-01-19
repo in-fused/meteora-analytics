0d4dcec1ce204e6363eab6b6db36dd8f7d735825
Infused <infusednft@gmail.com>
2026-01-19 18:08:42 -0500

    Create Aggregator.js

---
+++ public/js/aggregator/Aggregator.js
@@ -0,0 +1,15 @@
+import { Normalizer } from "./Normalizer.js";
+
+export const Aggregator = {
+  async fetchLivePools() {
+    const res = await fetch(`${CONFIG.METEORA_API}/pools`);
+    const raw = await res.json();
+
+    const out = [];
+    for (const p of raw) {
+      const n = await Normalizer.normalizePool(p);
+      if (n && n.freshness.ageMs < 30000) out.push(n);
+    }
+    return out;
+  }
+};
